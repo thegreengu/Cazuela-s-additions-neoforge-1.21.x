5e9d878256fbeaa669c65d45ee95093f0e2b0887
thegreengu <thegreencreator32@gmail.com>
2025-06-18 13:36:12 -0400

    Initial commit.

--- src/main/java/net/cazuelafria/cazuelasadditions/ExampleModClient.java
+++ src/main/java/net/cazuelafria/cazuelasadditions/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.cazuelafria.cazuelasadditions;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
@@ -7,7 +7,7 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = CazuelasAdditions.MOD_ID, dist = Dist.CLIENT)
 public class ExampleModClient {
     public ExampleModClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
